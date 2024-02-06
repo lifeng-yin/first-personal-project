9267afa31bf0cd40059d2cabc6a5f07677846764
Li Feng Yin <lifeng-yin@proton.me>
2024-02-05 19:18:25 -0500

    add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { Switch, Route, Redirect } from 'wouter'
 import Landing from './pages/Landing/Landing'
+import { ThemeProvider } from './contexts/theme-provider'
 
 
 
 function App() {
   return (
-    <Switch>
-      <Route path="/"><Landing /></Route>
-      <Route path="/login">Login Page</Route>
-      <Route path="/signup">Signup Page</Route>
-      <Route path="/app" nest>
-        <Switch>
-          <Route path="/"><Redirect to="/dashboard" /></Route>
-          <Route path="/dashboard">Dashboard</Route>
-          <Route>404</Route>
-        </Switch>
-      </Route>
-      <Route>404</Route>
-    </Switch>
+    <ThemeProvider>
+      <Switch>
+        <Route path="/"><Landing /></Route>
+        <Route path="/login">Login Page</Route>
+        <Route path="/signup">Signup Page</Route>
+        <Route path="/app" nest>
+          <Switch>
+            <Route path="/"><Redirect to="/dashboard" /></Route>
+            <Route path="/dashboard">Dashboard</Route>
+            <Route>404</Route>
+          </Switch>
+        </Route>
+        <Route>404</Route>
+      </Switch>
+    </ThemeProvider>
   )
 }
 
